4b4d54b57f9505c199ffbecccea267b1a6d5fb6d
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2024-03-14 21:17:41 +0200

    added new apiService

---
+++ src/app/api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DashboardInterface } from './types/dashboard.interface';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  url = 'https://user-assessment-api.vercel.app/';
+
+  private dashboardSubject = new BehaviorSubject<DashboardInterface[]>([]); // Поток данных о пользователях
+  dashboard$ = this.dashboardSubject.asObservable(); // Общедоступный Observable для подписки на поток данных
+  
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingSubject.asObservable();
+
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  fetchDashboard(): void {
+    this.isLoadingSubject.next(true);
+    const headers = this.authService.createHeadersWithToken();
+    const url = `${this.url}api/userassessments`;
+    this.http.get<DashboardInterface[]>(url, { headers: headers }).subscribe(
+      (response) => {
+        this.dashboardSubject.next(response);
+        this.isLoadingSubject.next(false);
+      },
+      (error) => {
+        console.error('Error fetching users:', error);
+      }
+    );
+  }
+}
